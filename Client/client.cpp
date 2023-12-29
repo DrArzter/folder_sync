14db63913aaf7cb41c629725d0d8b1fbd6a0f8be
Rostislav Chapega <chapegarostislav@gmail.com>
2023-12-29 16:18:16 +0100

    Update server and client. Add json configuration file

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <jsoncpp/json/json.h>
 #include <cstring>
 #include <cstdlib>
 #include <unistd.h>
@@ -6,16 +8,62 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <chrono>
+#include <thread>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+struct ClientConfig {
+    std::string serverIp;
+    std::string folderPath;
+};
+
+/**
+ * Reads the client configuration from the specified file.
+ *
+ * @param configFile the path to the configuration file
+ *
+ * @return the client configuration read from the file
+ *
+ * @throws std::ifstream::failure if the file cannot be opened
+ */
+ClientConfig readConfig(const std::string& configFile) {
+    ClientConfig config;
+    std::ifstream file(configFile);
+    if (file.is_open()) {
+        Json::Value root;
+        file >> root;
+
+        config.serverIp = root["serverIp"].asString();
+        config.folderPath = root["folderPath"].asString();
+    } else {
+        // Provide default values if the file cannot be opened
+        config.serverIp = "127.0.0.1";
+        config.folderPath = "./default_sync_folder";  // Default folder in the current directory
+        fs::create_directory(config.folderPath);      // Create the default folder
+    }
+
+    return config;
+}
 
 /**
  * This function creates a socket and connects to a server using TCP/IP.
+ *
+ * @param config the client configuration
  *
  * @return 0 if the connection is successful, 1 otherwise.
  *
  * @throws ErrorType if there is an error creating the socket, resolving the server's IP address,
  * connecting to the server, or receiving data from the server.
  */
-int main() {
+int startClient(const ClientConfig& config) {
+    // Check if the folder path exists, if not, create it
+    if (!fs::exists(config.folderPath)) {
+        fs::create_directory(config.folderPath);
+        std::cout << "Created folder: " << config.folderPath << std::endl;
+    }
+
     // Create a socket
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == -1) {
@@ -34,7 +82,7 @@ int main() {
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
-    if (getaddrinfo("127.0.0.1", nullptr, &hints, &res) != 0) {
+    if (getaddrinfo(config.serverIp.c_str(), nullptr, &hints, &res) != 0) {
         std::cerr << "Error resolving address." << std::endl;
         close(clientSocket);
         return 1;
@@ -49,6 +97,20 @@ int main() {
         return 1;
     }
 
+    // Send the folder path to the server
+    send(clientSocket, config.folderPath.c_str(), config.folderPath.size(), 0);
+
+    // Send last modification times of files to the server
+    for (const auto& entry : fs::recursive_directory_iterator(config.folderPath)) {
+        if (fs::is_regular_file(entry.path())) {
+            auto lastModified = fs::last_write_time(entry.path());
+            auto timePoint = std::chrono::time_point_cast<std::chrono::system_clock::duration>(lastModified - fs::file_time_type::clock::now() + std::chrono::system_clock::now());
+            std::time_t time_tValue = std::chrono::system_clock::to_time_t(timePoint);
+
+            send(clientSocket, &time_tValue, sizeof(time_tValue), 0);
+        }
+    }
+
     // Receive the synchronization result
     char buffer[1024];
     ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
@@ -61,6 +123,39 @@ int main() {
 
     // Close the client socket
     close(clientSocket);
+
+    return 0;
+}
+
+/**
+ * Main function that runs the program.
+ *
+ * @param argc the number of command line arguments
+ * @param argv an array of command line arguments
+ *
+ * @return 0 on success, 1 on failure
+ *
+ * @throws none
+ */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+        return 1;
+    }
+
+    std::string configFile = argv[1];
+    ClientConfig config = readConfig(configFile);
+
+    while (true) {
+        int result = startClient(config);
+        if (result != 0) {
+            // Handle errors if needed
+            std::cerr << "Error in client synchronization." << std::endl;
+        }
+
+        // Sleep for a second before the next synchronization
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     return 0;
 }
--- Server/server.cpp
+++ Server/server.cpp
@@ -1,59 +1,214 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <jsoncpp/json/json.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <filesystem>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
 #include <fstream>
+#include <chrono>
+#include <thread>
+#include <unordered_map>
+#include <set>
+#include <ctime> // Include ctime for difftime
+#include <filesystem>
 
 namespace fs = std::filesystem;
 
-/**
- * Synchronizes the folders from the source directory to the destination directory.
- *
- * @param sourceDir the path to the source directory
- * @param destDir the path to the destination directory
- * @param clientSocket the client socket used for communication
- *
- * @throws std::exception if an error occurs during synchronization
- */
-void synchronizeFolders(const std::string& sourceDir, const std::string& destDir, int clientSocket) {
-    try {
-        for (const auto& entry : fs::recursive_directory_iterator(sourceDir)) {
-            fs::path relativePath = fs::relative(entry.path(), sourceDir);
-            fs::path destPath = destDir / relativePath;
+#define MAX_PATH_LEN 1024
+#define MAX_BUFFER_SIZE 1024
 
-            if (fs::is_directory(entry.path())) {
+struct ServerConfig {
+    std::string serverSyncFolder;
+    int port;
+};
+
+/**
+ * Reads the server configuration from the specified file.
+ *
+ * @param configFile the path to the configuration file
+ *
+ * @return the server configuration read from the file
+ *
+ * @throws std::ifstream::failure if the file cannot be opened
+ */
+ServerConfig readConfig(const std::string& configFile) {
+    ServerConfig config;
+    std::ifstream file(configFile);
+    if (file.is_open()) {
+        Json::Value root;
+        file >> root;
+
+        if (root.isMember("serverSyncFolder")) {
+            config.serverSyncFolder = root["serverSyncFolder"].asString();
+        } else {
+            // Create a default folder in the current directory
+            config.serverSyncFolder = "./default_sync_folder";
+            mkdir(config.serverSyncFolder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        }
+
+        config.port = root.isMember("port") ? root["port"].asInt() : 12345;
+    } else {
+        // Provide default values if the file cannot be opened
+        config.serverSyncFolder = "./default_sync_folder";
+        mkdir(config.serverSyncFolder.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        config.port = 12345;
+    }
+
+    return config;
+}
+
+
+void synchronizeFolders(const char* clientSourceDir, const char* serverDestDir, int clientSocket) {
+    // Ensure the client source folder exists
+    if (!fs::exists(clientSourceDir)) {
+        fs::create_directory(clientSourceDir);
+        std::cout << "Created client source folder: " << clientSourceDir << std::endl;
+    }
+
+    // Ensure the server destination folder exists
+    if (!fs::exists(serverDestDir)) {
+        fs::create_directory(serverDestDir);
+        std::cout << "Created server destination folder: " << serverDestDir << std::endl;
+    }
+
+    // Sync from client to server
+    DIR* dir;
+    struct dirent* entry;
+
+    dir = opendir(clientSourceDir);
+    if (dir != NULL) {
+        while ((entry = readdir(dir)) != NULL) {
+            char relativePath[MAX_PATH_LEN];
+            snprintf(relativePath, sizeof(relativePath), "%s/%s", clientSourceDir, entry->d_name);
+
+            struct stat fileStat;
+            if (stat(relativePath, &fileStat) == -1) {
+                perror("Error getting file information");
+                continue;
+            }
+
+            if (S_ISDIR(fileStat.st_mode)) {
                 // Create directory in destination if it doesn't exist
-                fs::create_directories(destPath);
-            } else if (fs::is_regular_file(entry.path())) {
-                // Check if the file exists in the destination folder
-                if (fs::exists(destPath)) {
-                    // Compare modification times to determine if the file needs to be updated
-                    auto srcTime = fs::last_write_time(entry.path());
-                    auto destTime = fs::last_write_time(destPath);
-                    if (srcTime > destTime) {
+                char destPath[MAX_PATH_LEN];
+                snprintf(destPath, sizeof(destPath), "%s/%s", serverDestDir, entry->d_name);
+                mkdir(destPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+            } else if (S_ISREG(fileStat.st_mode)) {
+                // Compare modification times to determine if the file needs to be updated
+                char destPath[MAX_PATH_LEN];
+                snprintf(destPath, sizeof(destPath), "%s/%s", serverDestDir, entry->d_name);
+
+                int srcFile = open(relativePath, O_RDONLY);
+                int destFile = open(destPath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+                if (srcFile != -1 && destFile != -1) {
+                    struct stat srcStat, destStat;
+                    fstat(srcFile, &srcStat);
+                    fstat(destFile, &destStat);
+
+                    if (difftime(srcStat.st_mtime, destStat.st_mtime) > 0) {
                         // Copy the file if it's newer in the source folder
-                        fs::copy_file(entry.path(), destPath, fs::copy_options::overwrite_existing);
+                        char buffer[MAX_BUFFER_SIZE];
+                        ssize_t bytesRead, bytesWritten;
+
+                        while ((bytesRead = read(srcFile, buffer, sizeof(buffer))) > 0) {
+                            bytesWritten = write(destFile, buffer, bytesRead);
+                            if (bytesWritten == -1) {
+                                perror("Error writing to destination file");
+                                break;
+                            }
+                        }
                     }
-                } else {
-                    // File doesn't exist in destination, so copy it
-                    fs::copy_file(entry.path(), destPath);
+
+                    close(destFile);
+                }
+
+                if (srcFile != -1) {
+                    close(srcFile);
                 }
             }
         }
 
-        // Send a response to the client indicating the synchronization is complete
-        const char* response = "Synchronization complete.";
-        send(clientSocket, response, strlen(response), 0);
-    } catch (const std::exception& e) {
-        // Handle exceptions and send an error response to the client
-        std::cerr << "Error during synchronization: " << e.what() << std::endl;
-        const char* response = "Error during synchronization.";
-        send(clientSocket, response, strlen(response), 0);
+        closedir(dir);
     }
+
+    // Sync from server to client
+    DIR* serverDir;
+    struct dirent* serverEntry;
+
+    serverDir = opendir(serverDestDir);
+    if (serverDir != NULL) {
+        while ((serverEntry = readdir(serverDir)) != NULL) {
+            if (strcmp(serverEntry->d_name, ".") == 0 || strcmp(serverEntry->d_name, "..") == 0) {
+                continue; // Skip "." and ".."
+            }
+
+            char serverRelativePath[MAX_PATH_LEN];
+            snprintf(serverRelativePath, sizeof(serverRelativePath), "%s/%s", serverDestDir, serverEntry->d_name);
+
+            struct stat serverFileStat;
+            if (stat(serverRelativePath, &serverFileStat) == -1) {
+                perror("Error getting server file information");
+                continue;
+            }
+
+            if (S_ISDIR(serverFileStat.st_mode)) {
+                // Create directory in client if it doesn't exist
+                char clientDestPath[MAX_PATH_LEN];
+                snprintf(clientDestPath, sizeof(clientDestPath), "%s/%s", clientSourceDir, serverEntry->d_name);
+                mkdir(clientDestPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+            } else if (S_ISREG(serverFileStat.st_mode)) {
+                // Compare modification times to determine if the file needs to be updated
+                char clientDestPath[MAX_PATH_LEN];
+                snprintf(clientDestPath, sizeof(clientDestPath), "%s/%s", clientSourceDir, serverEntry->d_name);
+
+                int serverFile = open(serverRelativePath, O_RDONLY);
+                int clientDestFile = open(clientDestPath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+                if (serverFile != -1 && clientDestFile != -1) {
+                    struct stat serverStat, clientDestStat;
+                    fstat(serverFile, &serverStat);
+                    fstat(clientDestFile, &clientDestStat);
+
+                    if (difftime(serverStat.st_mtime, clientDestStat.st_mtime) > 0) {
+                        // Copy the file if it's newer on the server
+                        char buffer[MAX_BUFFER_SIZE];
+                        ssize_t bytesRead, bytesWritten;
+
+                        while ((bytesRead = read(serverFile, buffer, sizeof(buffer))) > 0) {
+                            bytesWritten = write(clientDestFile, buffer, bytesRead);
+                            if (bytesWritten == -1) {
+                                perror("Error writing to client destination file");
+                                break;
+                            }
+                        }
+                    }
+
+                    close(clientDestFile);
+                }
+
+                if (serverFile != -1) {
+                    close(serverFile);
+                }
+            }
+        }
+
+        closedir(serverDir);
+    }
+
+    // Send a response to the client indicating the synchronization is complete
+    const char* response = "Bidirectional Synchronization complete.";
+    send(clientSocket, response, strlen(response), 0);
+
+    // Print to console
+    std::cout << "Bidirectional Synchronization complete for folder: " << clientSourceDir << std::endl;
 }
+
 
 /**
  * Main function that creates a socket, binds it to a specific IP and port, listens for incoming connections,
@@ -64,7 +219,15 @@ void synchronizeFolders(const std::string& sourceDir, const std::string& destDir
  * @throws ErrorType if there is an error creating the socket, binding the socket, listening for connections,
  * accepting a connection, or handling synchronization.
  */
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+        return 1;
+    }
+
+    std::string configFile = argv[1];
+    ServerConfig config = readConfig(configFile);
+
     // Create a socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
@@ -76,7 +239,7 @@ int main() {
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(12345); // Change to the desired port number
+    serverAddress.sin_port = htons(config.port);
 
     if (bind(serverSocket, reinterpret_cast<struct sockaddr*>(&serverAddress), sizeof(serverAddress)) == -1) {
         std::cerr << "Error binding socket." << std::endl;
@@ -100,9 +263,24 @@ int main() {
             return 1;
         }
 
-        // Handle synchronization for each connection in a separate thread or process
-        // For simplicity, we'll handle synchronization in the same thread here
-        synchronizeFolders("/path/to/source", "/path/to/destination", clientSocket);
+        // Receive the client's folder path
+        char folderPathBuffer[MAX_PATH_LEN];
+        ssize_t pathBytesRead = recv(clientSocket, folderPathBuffer, sizeof(folderPathBuffer), 0);
+        if (pathBytesRead == -1) {
+            std::cerr << "Error receiving folder path from client." << std::endl;
+            close(clientSocket);
+            continue;
+        }
+        folderPathBuffer[pathBytesRead] = '\0';
+
+        // Check if the folder exists, if not, create it
+        if (!fs::exists(folderPathBuffer)) {
+            fs::create_directory(folderPathBuffer);
+            std::cout << "Created folder: " << folderPathBuffer << std::endl;
+        }
+
+        // Handle bidirectional synchronization for each connection
+        synchronizeFolders(folderPathBuffer, config.serverSyncFolder.c_str(), clientSocket);
 
         // Close the client socket
         close(clientSocket);
